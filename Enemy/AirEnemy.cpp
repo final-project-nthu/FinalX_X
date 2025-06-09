391bc3d7e9567ca1e992582404cce3df1b0e8964
Answer0314 <zedorfedhaha047@gmail.com>
2025-06-09 19:54:12 +0800

    Add files via upload
    我的enemy

---
+++ Enemy/AirEnemy.cpp
@@ -0,0 +1,8 @@
+#include <string>
+
+#include "AirEnemy.hpp"
+
+// TODO HACKATHON-3 (1/3): You can imitate the 2 files: 'SoldierEnemy.hpp', 'SoldierEnemy.cpp' to create a new enemy.
+AirEnemy::AirEnemy(int x,int y):Enemy("play/enemy-2.png",x,y,30,100,20,7){
+
+}
