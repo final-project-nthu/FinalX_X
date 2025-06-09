7d8cc301858bf39d003cd8011d6513286f93d25e
Answer0314 <zedorfedhaha047@gmail.com>
2025-06-09 20:37:44 +0800

    Delete Enemy directory

--- Enemy/AirEnemy.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-#include "AirEnemy.hpp"
-
-// TODO HACKATHON-3 (1/3): You can imitate the 2 files: 'SoldierEnemy.hpp', 'SoldierEnemy.cpp' to create a new enemy.
-AirEnemy::AirEnemy(int x,int y):Enemy("play/enemy-2.png",x,y,30,100,20,7){
-
-}
