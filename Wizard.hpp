843c93494e499019a7107e188289399678fe44e2
PowerLun <Yulun566@gmail.com>
2025-06-11 21:59:07 +0800

    Update Wizard.hpp

--- Wizard.hpp
+++ Wizard.hpp
@@ -1,16 +1,13 @@
+// Wizard.hpp
 #ifndef WIZARD_HPP
 #define WIZARD_HPP
+#include "Player.hpp"
+#include <string>
 
-#include "Player/Player.hpp"
-#include<string>
 class Wizard : public Player {
 public:
-    Wizard(std::string imgBase, std::string imgPlayer, float x, float y, float radius, float speed);
-
+    Wizard(std::string img, float x, float y, float speed);
     void Update(float deltaTime) override;
-
-protected:
-    // 技能按鍵事件，現在先空著
     void CastQ();
     void CastE();
     void CastR();
