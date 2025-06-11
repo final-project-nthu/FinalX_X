b064b5f52ae169a3e87073fe282730f3dba06f8d
PowerLun <Yulun566@gmail.com>
2025-06-11 21:58:53 +0800

    Update Wizard.cpp

--- Wizard.cpp
+++ Wizard.cpp
@@ -1,36 +1,28 @@
-#include <allegro5/allegro.h>
+// Wizard.cpp
 #include "Player/Wizard.hpp"
-#include "Engine/GameEngine.hpp"
+#include <allegro5/allegro.h>
 
-Wizard::Wizard(std::string imgBase, std::string imgPlayer, float x, float y, float radius, float speed)
-    : Player(imgBase, imgPlayer, x, y, radius, speed) {
+Wizard::Wizard(std::string img, float x, float y, float speed)
+    : Player(img, x, y, speed) {
 }
 
 void Wizard::Update(float deltaTime) {
-    Player::Update(deltaTime);  // 繼承Player的Update做移動、射擊等基本行為
-
+    Player::Update(deltaTime);
     ALLEGRO_KEYBOARD_STATE keyState;
     al_get_keyboard_state(&keyState);
-
-    if (al_key_down(&keyState, ALLEGRO_KEY_Q)) {
-        CastQ();
-    }
-    if (al_key_down(&keyState, ALLEGRO_KEY_E)) {
-        CastE();
-    }
-    if (al_key_down(&keyState, ALLEGRO_KEY_R)) {
-        CastR();
-    }
+    if (al_key_down(&keyState, ALLEGRO_KEY_Q)) CastQ();
+    if (al_key_down(&keyState, ALLEGRO_KEY_E)) CastE();
+    if (al_key_down(&keyState, ALLEGRO_KEY_R)) CastR();
 }
 
 void Wizard::CastQ() {
-    // 這裡放 Q 技能的程式碼，暫時空白
+    // Q skill placeholder
 }
 
 void Wizard::CastE() {
-    // 這裡放 E 技能的程式碼，暫時空白
+    // E skill placeholder
 }
 
 void Wizard::CastR() {
-    // 這裡放 R 技能的程式碼，暫時空白
+    // R skill placeholder
 }
